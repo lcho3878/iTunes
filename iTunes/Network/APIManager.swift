9c869b394204d222f4ac088e4009a1ca39e5d269
lcho3878 <leech3878@naver.com>
2024-08-12 17:16:24 +0900

    Feat: Single객체를 이용한 네트워크 통신

--- iTunes/Network/APIManager.swift
+++ iTunes/Network/APIManager.swift
@@ -14,16 +14,14 @@ final class APIManager {
     
     private init() {}
     
-    func callRequest(term: String) -> Observable<MediaResult> {
-//        let url = "https://itunes.apple.com/search?"
+    func callRequest(term: String) -> Single<MediaResult> {
         let url = "https://itunes.apple.com/search?term=\(term)&country=KR&lang=ko_KR&media=movie"
-//        term=%ED%83%9D%EC%8B%9C&country=KR&lang=ko_KR
-        let result = Observable<MediaResult>.create { observer in
+        let result = Single<MediaResult>.create { single in
             let sessionConfig = URLSessionConfiguration.default
             sessionConfig.httpAdditionalHeaders = ["User-Agent": "dsaf"]
             let session = URLSession(configuration: sessionConfig)
             guard let url = URL(string: url) else {
-                observer.onError(APIError.invalidURL)
+                single(.failure(APIError.invalidURL))
                 return Disposables.create()
             }
             var urlRequest = URLRequest(url: url)
@@ -33,21 +31,20 @@ final class APIManager {
 
             session.dataTask(with: url) { data, response, error in
                 guard error == nil else {
-                    observer.onError(APIError.unknownResponse)
+                    single(.failure(APIError.unknownResponse))
                     return
                 }
                 guard let response = response as? HTTPURLResponse ,
                     (200...299).contains(response.statusCode) else {
-                    observer.onError(APIError.statusError)
+                    single(.failure(APIError.statusError))
                     return
                 }
                 if let data = data,
                    let appData = try? JSONDecoder().decode(MediaResult.self, from: data) {
-                    observer.onNext(appData)
-                    observer.onCompleted()
+                    single(.success(appData))
                 }
                 else {
-                    observer.onError(APIError.decodingFail)
+                    single(.failure(APIError.decodingFail))
                 }
             }
             .resume()
